ebac1e21e90632ef0f96891024f6fbada6460b75
Zachary McDanal <zlmcdanal@student.fullsail.edu>
2022-10-26 14:37:28 -0500

    Material3 Addition

--- ibottaOffersTest/app/src/main/java/com/ethereal/ibottaofferstest/screens/OffersGridScreen.kt
+++ ibottaOffersTest/app/src/main/java/com/ethereal/ibottaofferstest/screens/OffersGridScreen.kt
@@ -1,56 +1,58 @@
 package com.ethereal.ibottaofferstest.screens
 
-import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.ethereal.ibottaofferstest.R
 import com.ethereal.ibottaofferstest.components.OfferListItemCard
-import com.ethereal.ibottaofferstest.objects.Offer
 import com.ethereal.ibottaofferstest.routes.Routes
 import com.ethereal.ibottaofferstest.ui.theme.IbottaOffersTestTheme
-import com.ethereal.ibottaofferstest.utilities.viewModelProviderFactoryOf
 import com.ethereal.ibottaofferstest.view_models.LocalNavController
 import com.ethereal.ibottaofferstest.view_models.OffersViewModel
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.JsonDataException
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import java.io.ByteArrayOutputStream
 
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun OffersGridScreen(offersViewModel: OffersViewModel) {
-    val context = LocalContext.current
     val navController = LocalNavController.current
-
-
-
-    LazyVerticalGrid(
-        verticalArrangement = Arrangement.spacedBy(24.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        cells = GridCells.Fixed(2),
-        contentPadding = PaddingValues(start = 8.dp, top = 24.dp, end = 8.dp, bottom = 24.dp),
-        modifier = Modifier.fillMaxSize()
+    
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.background(color = MaterialTheme.colors.onBackground),
+                title = {
+                    Text(text = "Offers", color = Color.Black)
+                }
+            )
+        }
     ) {
-        items(offersViewModel.getOffers()) { item ->
-            OfferListItemCard(offer = item) {
-                offersViewModel.toggleFavorite(item)
-                navController.navigate(Routes.DetailsScreen.route)
+
+        LazyVerticalGrid(
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 8.dp, top = 24.dp, end = 8.dp, bottom = 24.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(offersViewModel.getOffers()) { item ->
+                OfferListItemCard(offer = item) {
+                    offersViewModel.toggleFavorite(item)
+                    navController.navigate(Routes.DetailsScreen.route)
+                }
             }
         }
     }
